aba90c6a8571f8a04d83765444cc354631c27e41
giordanozanetti <giordanogoulartzanetti@gmail.com>
2025-09-23 22:46:53 -0300

    Add files via upload

--- projeto_quimica/js/script.js
+++ projeto_quimica/js/script.js
@@ -1,55 +1,54 @@
-// function checkAnswer(question, answear) {
-//     let inputSelected = document.querySelector(input[name="q${question}"]:checked);
-//     feedback.style.display = "block";
-
-//     if (inputSelected?.value == answear) {
-//         feedback.innerHTML = '<p class="correct back">✅ Parabéns! Resposta correta!</p>';
-//         info.style.display = 'flex';
-
-//         let options = document.querySelectorAll(input[name="q${question}"]);
-
-//         for (let i = 0; i < options.length; i++) {
-//             options[i].disabled = true;
-//         };
-
-//         document.querySelector(#respond${question}).disabled = true;
-
-//         if (question == questions.length) {
-//             setTimeout(() => {
-//                 document.querySelector("#quiz-complete").classList.add('show');
-//             }, 2000)
-//         } else {
-//             setTimeout(() => {
-//                 document.querySelector(#question${question + 1}).classList.add('show');
-//             }, 2500);
-//         }
-//     } else if (inputSelected == null) {
-        
-//     } else {
-        
-//     }
-// }
-
-
-
-
-
-
-
-
-
+let perguntas = document.querySelectorAll(".question");
 function verifica(questao, resposta) {
     let resposta_selecionada = document.querySelector(`input[name="${questao}"]:checked`);
     let numero = parseInt(questao[1])
+    function none() {
+        
+        document.querySelector(`#acerto${numero}`).style.display = 'none';
+        document.querySelector(`#info${numero}`).style.display = 'none';
+        document.querySelector(`#pergunta_${numero}`).style.display = 'none';
+        document.querySelector(`#titulo${numero}`).style.display = 'none';
+        document.querySelector(`#infos_start`).style.display ='none'
+    }
+    function show() {
+        document.querySelector(`#info${numero + 1}`).classList.add('aparece');
+        document.querySelector(`#pergunta_${numero + 1}`).classList.add('aparece');
+        document.querySelector(`#titulo${numero + 1}`).classList.add('aparece');
+        document.querySelector(`#infos_start`).classList.add('aparece');
 
+    }
     if (resposta_selecionada?.id == resposta) {
+
+        document.querySelector(`#responda${numero}`).style.display = 'none'
+        document.querySelector(`#erro${numero}`).style.display = 'none'
+
+        document.querySelector(`#info${numero}`).style.display = 'flex';
+        document.querySelector(`#pergunta_${numero}`).style.display = 'flex';
+        document.querySelector(`#titulo${numero}`).style.display = 'flex';
+        document.querySelector(`#infos_start`).style.document = 'flex';
+
+
+        document.querySelector(`#info${numero}`).classList.remove('aparece');
+        document.querySelector(`#pergunta_${numero}`).classList.remove('aparece');
+        document.querySelector(`#titulo${numero}`).classList.remove('aparece');
+        document.querySelector(`#infos_start`).classList.remove('aparece');
+        document.querySelector(`#infos_start`).style.display = 'flex';
+        document.querySelector(`#acerto${numero}`).style.display = 'flex';
+
+        document.querySelector(`#infos_start`).classList.add('some');
         document.querySelector(`#info${numero}`).classList.add('some');
         document.querySelector(`#pergunta_${numero}`).classList.add('some');
         document.querySelector(`#titulo${numero}`).classList.add('some');
-        
-        document.querySelector(`#info${numero+1}`).classList.add('aparece');
-        document.querySelector(`#pergunta_${numero+1}`).classList.add('aparece');
-        document.querySelector(`#titulo${numero+1}`).classList.add('aparece');
+        document.querySelector(`#acerto${numero}`).classList.add('some')
+
+
+        setTimeout(none, 800)
+        if (numero < perguntas.length) {
+            setTimeout(show, 800)
+        }
+        else {
+            document.querySelector(`#parabens`).classList.add('aparece');
+        }
     }
     else if (resposta_selecionada == null) {
         document.querySelector(`#acerto${numero}`).style.display = 'none'
@@ -60,5 +59,6 @@ function verifica(questao, resposta) {
         document.querySelector(`#responda${numero}`).style.display = 'none'
         document.querySelector(`#erro${numero}`).style.display = 'flex'
     }
+
 }
 ;
